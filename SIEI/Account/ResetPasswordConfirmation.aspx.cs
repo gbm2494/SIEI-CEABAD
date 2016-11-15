3d4890c251a22042d561b0e7149c3651eea8669b
devmyrrie <devmyrrie@gmail.com>
2016-11-15 11:30:03 -0600

    Project init

---
+++ SIEI/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SIEI.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SIEI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIEI.Startup))]
+namespace SIEI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
