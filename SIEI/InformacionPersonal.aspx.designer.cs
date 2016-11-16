9141639cf4098cedb4f7b59c3dbf4485eb538816
devmyrrie <devmyrrie@gmail.com>
2016-11-16 08:53:58 -0600

    Vista terminada informacion personal

--- SIEI/InformacionPersonal.aspx.designer.cs
+++ SIEI/InformacionPersonal.aspx.designer.cs
@@ -137,5 +137,23 @@ namespace SIEI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label disc;
+        
+        /// <summary>
+        /// txtCorreo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+        
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
     }
 }
