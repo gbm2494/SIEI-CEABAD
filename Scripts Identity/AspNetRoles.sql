5ba126a6d325af66ee02e07e31558a68583c1c12
gbm2494 <gbm2494@hotmail.com>
2016-11-16 09:31:29 -0600

    queries para asp.net identity

---
+++ Scripts Identity/AspNetRoles.sql
@@ -0,0 +1,22 @@
+USE [SIEI]
+GO
+
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 16/11/2016 09:29:32 a.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
